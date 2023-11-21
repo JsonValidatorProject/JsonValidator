0fcf97cf9d74fc44ecd3d9a9e9e6231db9dc6cc3
Balázs Kis <uibali@gmail.com>
2023-11-21 17:16:12 +0100

    fix type cache (using concurrent dictionary)

--- JsonValidator/JsonElementExtensions.cs
+++ JsonValidator/JsonElementExtensions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -8,7 +9,7 @@ namespace JsonValidator;
 
 public static class JsonElementExtensions
 {
-    private static readonly Dictionary<Type, PropertyInfo[]> TypePropertiesCache = new();
+    private static readonly ConcurrentDictionary<Type, PropertyInfo[]> TypePropertiesCache = new();
 
     private static readonly HashSet<Type> PrimitiveTypes =
     [
