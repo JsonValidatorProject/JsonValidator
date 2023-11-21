468217add52359042b05449905e70d764705fe53
Balázs Kis <uibali@gmail.com>
2023-11-21 16:22:32 +0100

    adding property tests

--- JsonValidator.Tests/SimpleValueTests.cs
+++ JsonValidator.Tests/SimpleValueTests.cs
@@ -1,4 +1,4 @@
-﻿using System.Globalization;
+﻿using System.Collections;
 
 namespace JsonValidator.Tests;
 
@@ -6,138 +6,57 @@ public class SimpleValueTests
 {
     public class MatchTests
     {
-        [Fact]
-        public void TestStringMatch() => JsonDocument.Parse("\"example\"").ValidateMatch("example");
+        private class MatchTestData : IEnumerable<object[]>
+        {
+            public IEnumerator<object[]> GetEnumerator()
+            {
+                yield return new object[] { "\"example\"", "example" };
+                yield return new object[] { "true", true };
+                yield return new object[] { "1024", (short)1024 };
+                yield return new object[] { "1024", 1024 };
+                yield return new object[] { "1024", (long)1024 };
+                yield return new object[] { "975.4527", (float)975.4527 };
+                yield return new object[] { "975.4527", 975.4527 };
+                yield return new object[] { "975.4527", (decimal)975.4527 };
+            }
 
-        [Fact]
-        public void TestBooleanMatch() => JsonDocument.Parse("true").ValidateMatch(true);
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
 
-        [Fact]
-        public void TestShortMatch() => JsonDocument.Parse("1024").ValidateMatch((short)1024);
-
-        [Fact]
-        public void TestIntMatch() => JsonDocument.Parse("1024").ValidateMatch(1024);
-
-        [Fact]
-        public void TestLongMatch() => JsonDocument.Parse("1024").ValidateMatch((long)1024);
-
-        [Fact]
-        public void TestFloatMatch() => JsonDocument.Parse("975.4527").ValidateMatch((float)975.4527);
-
-        [Fact]
-        public void TestDoubleMatch() => JsonDocument.Parse("975.4527").ValidateMatch(975.4527);
-
-        [Fact]
-        public void TestDecimalMatch() => JsonDocument.Parse("975.4527").ValidateMatch((decimal)975.4527);
+        [Theory]
+        [ClassData(typeof(MatchTestData))]
+        public void TestMatch(string json, object expectedObject) =>
+            JsonDocument.Parse(json).ValidateMatch(expectedObject);
     }
 
     public class MismatchTests
     {
-        [Fact]
-        public void TestStringMismatch()
+        private class MismatchTestData : IEnumerable<object[]>
         {
-            const string jsonValue = "wrong";
-            const string expectedValue = "example";
+            public IEnumerator<object[]> GetEnumerator()
+            {
+                yield return new object[] { "\"wrong\"", "example", "wrong", "example" };
+                yield return new object[] { "false", true, false, true };
+                yield return new object[] { "2048", (short)1024, 2048, 1024 };
+                yield return new object[] { "2048", 1024, 2048, 1024 };
+                yield return new object[] { "2048", (long)1024, 2048, 1024 };
+                yield return new object[] { "575.4527", (float)975.4527, 575.4527, 975.4527 };
+                yield return new object[] { "575.4527", 975.4527, 575.4527, 975.4527 };
+                yield return new object[] { "575.4527", (decimal)975.4527, 575.4527, 975.4527 };
+            }
 
-            void Act() => JsonDocument.Parse($"\"{jsonValue}\"").ValidateMatch(expectedValue);
-
-            var exception = Assert.Throws<ValidationFailedException>(Act);
-            Assert.Contains(jsonValue, exception.Message);
-            Assert.Contains(expectedValue, exception.Message);
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
         }
 
-        [Fact]
-        public void TestBooleanMismatch()
+        [Theory]
+        [ClassData(typeof(MismatchTestData))]
+        public void TestStringMismatch(string json, object expectedObject, object jsonValue, object expectedValue)
         {
-            const bool jsonValue = false;
-            const bool expectedValue = true;
-
-            void Act() => JsonDocument.Parse(jsonValue.ToString().ToLower()).ValidateMatch(expectedValue);
+            void Act() => JsonDocument.Parse(json).ValidateMatch(expectedObject);
 
             var exception = Assert.Throws<ValidationFailedException>(Act);
-            Assert.Contains(jsonValue.ToString(), exception.Message);
-            Assert.Contains(expectedValue.ToString(), exception.Message);
-        }
-
-        [Fact]
-        public void TestShortMismatch()
-        {
-            const short jsonValue = 2048;
-            const short expectedValue = 1024;
-
-            void Act() => JsonDocument.Parse(jsonValue.ToString()).ValidateMatch(expectedValue);
-
-            var exception = Assert.Throws<ValidationFailedException>(Act);
-            Assert.Contains(jsonValue.ToString(), exception.Message);
-            Assert.Contains(expectedValue.ToString(), exception.Message);
-        }
-
-        [Fact]
-        public void TestIntMismatch()
-        {
-            const int jsonValue = 2048;
-            const int expectedValue = 1024;
-
-            void Act() => JsonDocument.Parse(jsonValue.ToString()).ValidateMatch(expectedValue);
-
-            var exception = Assert.Throws<ValidationFailedException>(Act);
-            Assert.Contains(jsonValue.ToString(), exception.Message);
-            Assert.Contains(expectedValue.ToString(), exception.Message);
-        }
-
-        [Fact]
-        public void TestLongMismatch()
-        {
-            const long jsonValue = 2048;
-            const long expectedValue = 1024;
-
-            void Act() => JsonDocument.Parse(jsonValue.ToString()).ValidateMatch(expectedValue);
-
-            var exception = Assert.Throws<ValidationFailedException>(Act);
-            Assert.Contains(jsonValue.ToString(), exception.Message);
-            Assert.Contains(expectedValue.ToString(), exception.Message);
-        }
-
-        [Fact]
-        public void TestFloatMismatch()
-        {
-            const float jsonValue = 575.4527f;
-            const float expectedValue = 975.4527f;
-
-            void Act() =>
-                JsonDocument.Parse(jsonValue.ToString(CultureInfo.InvariantCulture)).ValidateMatch(expectedValue);
-
-            var exception = Assert.Throws<ValidationFailedException>(Act);
-            Assert.Contains(jsonValue.ToString(CultureInfo.InvariantCulture), exception.Message);
-            Assert.Contains(expectedValue.ToString(CultureInfo.InvariantCulture), exception.Message);
-        }
-
-        [Fact]
-        public void TestDoubleMismatch()
-        {
-            const double jsonValue = 575.4527;
-            const double expectedValue = 975.4527;
-
-            void Act() =>
-                JsonDocument.Parse(jsonValue.ToString(CultureInfo.InvariantCulture)).ValidateMatch(expectedValue);
-
-            var exception = Assert.Throws<ValidationFailedException>(Act);
-            Assert.Contains(jsonValue.ToString(CultureInfo.InvariantCulture), exception.Message);
-            Assert.Contains(expectedValue.ToString(CultureInfo.InvariantCulture), exception.Message);
-        }
-
-        [Fact]
-        public void TestDecimalMismatch()
-        {
-            const decimal jsonValue = 575.4527m;
-            const decimal expectedValue = 975.4527m;
-
-            void Act() =>
-                JsonDocument.Parse(jsonValue.ToString(CultureInfo.InvariantCulture)).ValidateMatch(expectedValue);
-
-            var exception = Assert.Throws<ValidationFailedException>(Act);
-            Assert.Contains(jsonValue.ToString(CultureInfo.InvariantCulture), exception.Message);
-            Assert.Contains(expectedValue.ToString(CultureInfo.InvariantCulture), exception.Message);
+            Assert.Contains(jsonValue.ToString()!, exception.Message);
+            Assert.Contains(expectedValue.ToString()!, exception.Message);
         }
     }
 }
